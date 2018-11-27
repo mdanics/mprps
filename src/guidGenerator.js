c53bfeee793b101a5c5d19a8a0267f0825c057e6
mdanics <matthew.danics@tapmango.com>
2018-11-26 22:34:57 -0500

    Update guidGenerator.js

--- src/guidGenerator.js
+++ src/guidGenerator.js
@@ -2,6 +2,7 @@
  * Created by mdanics18 on 2017-12-17.
  */
 
+/* Old method which was unique to a user and their browser. The problem with this was that you were unable to play agasint yourself in a new tab or window. 
 function userGuid() {
 
     var nav = window.navigator;
@@ -14,6 +15,16 @@ function userGuid() {
     guid += screen.pixelDepth || '';
 
     return guid;
+} */
+
+function userGuid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+
 }
 
 export {userGuid}
